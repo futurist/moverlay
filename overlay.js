d3bb4098e1c1f60e79dfdaa33d6d7067f61b9d90
imac <26634873@qq.com>
2016-05-23 07:31:16 +0800

    ADD: IE8 support & polyfill

--- overlay.js
+++ overlay.js
@@ -1,3 +1,5 @@
+
+
 /**
  * @fileOverview
  * @name overlay.js
@@ -14,42 +16,45 @@ var overlay = {
       ctrl.width = _getWindowSize()[0]
       ctrl.height = _getWindowSize()[1]
       m.redraw()
-    },
+    }
     window.addEventListener('resize', onresize)
     onresize()
   },
   view: function (ctrl) {
-    return m( 'table.overlay',
-              {
-                style:{
-                  border:0,
-                  padding:0,
-                  margin:get(),
-                  width:ctrl.width+'px',
-                  height:ctrl.height+'px',
-                  backgroundColor:'rgba(0,0,0,0.5)'
-                }
-              },
-              m( 'tr',
-                 m( 'td',
-                    {
-                      align:'center',
-                      style:{
-                        verticalAlign:'middle'
-                      }
-                    },
-                    m( 'div',
-                       {
-                         style:{
-                           width:'200px'
-                         }
-                       },
-                       "soidjfoj\nosdijf<br>sodj23423foij"
-                     )
+    // return m('div', 'content of abc')
+    console.log("oisdjfioj", _getWindowSize())
+    return m('table.overlay',
+             {
+               style: {
+                 border: 0+'px',
+                 padding: 0+'px',
+                 margin: 0+'px',
+                 width: ctrl.width + 'px',
+                 height: ctrl.height + 'px',
+                 // using below format to supress error in IE8
+                 'background-color': 'rgba(0,0,0,0.5)'
+               }
+             },
+             m('tr',
+               m('td',
+                 {
+                   align: 'center',
+                   style: {
+                     verticalAlign: 'middle'
+                   }
+                 },
+                 m('div',
+                   {
+                     style: {
+                       width: '200px'
+                     }
+                   },
+                   'soidjfoj\nosdijf<br>sodj23423foij'
                   )
-               )
+                )
+              )
             )
   }
 }
 
-m.mount(document.body, overlay)
+m.mount(document.getElementById('abc'), overlay)
